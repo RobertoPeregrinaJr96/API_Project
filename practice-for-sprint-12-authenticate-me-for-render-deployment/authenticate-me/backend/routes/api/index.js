15b2c983a2c70e3a4e9b458284f4f9e5ea9e64d8
Roberto Peregrina <111211515+RobertoPeregrinaJr96@users.noreply.github.com>
2023-04-13 11:32:09 -0700

    Add User login backend endpoin

--- practice-for-sprint-12-authenticate-me-for-render-deployment/authenticate-me/backend/routes/api/index.js
+++ practice-for-sprint-12-authenticate-me-for-render-deployment/authenticate-me/backend/routes/api/index.js
@@ -4,8 +4,17 @@ const { User } = require('../../db/models');
 const { restoreUser } = require('../../utils/auth.js');
 const { requireAuth } = require('../../utils/auth.js');
 
+const sessionRouter = require('./session.js');
+const usersRouter = require('./users.js');
+
+
+
 
 router.use(restoreUser);
+
+router.use('/session', sessionRouter);
+
+router.use('/users', usersRouter);
 
 
 // POST /api/test
