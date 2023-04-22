a0125894a96d52946513803763fe0bdd231f4ece
Roberto Peregrina <111211515+RobertoPeregrinaJr96@users.noreply.github.com>
2023-04-22 15:14:44 -0700

    Update booking.js

--- backend/db/models/booking.js
+++ backend/db/models/booking.js
@@ -11,8 +11,8 @@ module.exports = (sequelize, DataTypes) => {
      */
     static associate(models) {
       // define association here
-      Booking.belongsTo(models.User, { foreignKey: 'userId', onDelete: 'CASCADE', otherKey: 'id' })
-      Booking.belongsTo(models.Spot, { foreignKey: 'spotId', otherKey: 'id', onDelete: 'CASCADE' })
+      Booking.belongsTo(models.User, { through: 'User', foreignKey: 'userId', onDelete: 'CASCADE', otherKey: 'id' })
+      Booking.belongsTo(models.Spot, { through: 'Spot', foreignKey: 'spotId', otherKey: 'id', onDelete: 'CASCADE' })
     }
   }
   Booking.init({
@@ -34,11 +34,11 @@ module.exports = (sequelize, DataTypes) => {
     },
     userId: {
       type: DataTypes.INTEGER,
-      allowNull:true
+      allowNull: false
     },
     spotId: {
       type: DataTypes.INTEGER,
-      allowNull:true
+      allowNull: false
     }
   }, {
     sequelize,
