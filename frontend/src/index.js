5ba0386780538c8b07ffd68549d9b4572aeb5aeb
Roberto_peregrina_Jr <robertoperegrina1996@gmail.com>
2023-05-04 19:09:01 -0700

    CRA installed

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,4 +1,30 @@
-const React = require('react');
-const ReactDOM = require('react-dom')
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { BrowserRouter } from 'react-router-dom';
+import { Provider as ReduxProvider } from 'react-redux';
+import './index.css';
+import App from './App';
+import configureStore from './store';
 
-ReactDOM.render(<h1>Hello World</h1>, document.getElementById('root'));
+const store = configureStore();
+
+if (process.env.NODE_ENV !== "production") {
+  window.store = store;
+}
+
+function Root() {
+  return (
+    <ReduxProvider store={store}>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </ReduxProvider>
+  );
+}
+
+ReactDOM.render(
+  <React.StrictMode>
+    <Root />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
